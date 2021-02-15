b5575989e5a70a63b66628f6fd00310674290d93
Shoncito92 <cromaticsoul@gmail.com>
2021-02-14 23:55:07 -0300

    Cambios casi finales

--- app_proyecto_medico/urls.py
+++ app_proyecto_medico/urls.py
@@ -6,13 +6,14 @@ app_name = "app_proyecto_medico"
 urlpatterns = [
     path('', views.index, name='index'),
     path('privada', views.privada, name='privada'),
-    path('graficos', views.graficos, name='graficos'),
     path('examenes', views.examenes, name='examenes'),
     path('agendar', views.agendar, name='agendar'),
     path('login', views.login, name='login'),
     path('registro', views.registro, name='registro'),
     path('crear_paciente', views.crear_paciente, name='crear_paciente'),
     path('lista_paciente', views.lista_paciente, name='lista_paciente'),
+    path('<identificador>/eliminar_examen', views.eliminar_examen, name='eliminar_examen'),
     path('<identificador>/eliminar_paciente', views.eliminar_paciente, name='eliminar_paciente'),
+    path('graficos', views.graficos, name='graficos'),
     
 ]
--- app_proyecto_medico/views.py
+++ app_proyecto_medico/views.py
@@ -9,42 +9,99 @@ import datetime
 import json
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'app_proyecto_medico/index.html')
-def privada(request):
-    return render(request, 'app_proyecto_medico/privada.html')
 
-    
+
+def privada(request):
+    if request.method == "GET":
+        print("El request GET: ", request.GET)
+        if 'id' in request.GET:
+            id = request.GET['id']
+            print("El id del GET es: ", id)
+        filename = "/app_proyecto_medico/data/examen.json"
+        with open(str(settings.BASE_DIR)+filename, 'r') as file:
+            data = json.load(file)
+            #print("Esto es data", data)
+        for paciente in data['formulario']:
+            cliente = paciente['nombre'], paciente['identificador']
+            #print("el cliente tiene:",  cliente)
+
+        context = {'lista_paciente': data['formulario']}
+
+        print("El context contiene", context)
+        return render(request, 'app_proyecto_medico/privada.html', context)
+
+    elif request.method == "POST":
+        print("El request GET: ", request.GET)
+        if 'id' in request.GET:
+            id = request.GET['id']
+            print("El id en el GET es: ", id)
+
+        filename = '/app_proyecto_medico/data/examen.json'
+        with open(str(settings.BASE_DIR)+filename, 'r') as file:
+            data = json.load(file)
+        for paciente in data['formulario']:
+            if int(paciente['identificador']) == int(id):
+                print(paciente['nombre'], paciente['rut'])
+                perfil = {'perfil': paciente}
+                print("Estos son los datos del paciente: ", paciente)
+
+        context = {'lista_paciente': data['formulario']}
+        context.update(perfil)
+        print("El context con update tiene:", context)
+        return render(request, 'app_proyecto_medico/privada.html', context)
+
+
 def graficos(request):
-    return render(request, 'app_proyecto_medico/graficos.html')
-    
-def context_lista_examen():
-    filename= "/app_proyecto_medico/data/examen.json"
+    lista_examen = []
+    lista_medico = []
+    filename = "/app_proyecto_medico/data/examen.json"
     with open(str(settings.BASE_DIR)+filename, 'r') as file:
-        formulario=json.load(file)
+        lista_pacientes = json.load(file)
+        for elemento in lista_pacientes.get('formulario')[-5:]:
+            examen = elemento.get('examen')
+            medico = elemento.get('medico')
+            lista_examen.append(examen)
+            lista_medico.append(medico)
+    print(lista_examen)
+    print(lista_medico)
+    context = {'examen': lista_examen, 'medico': lista_medico}
+    return render(request, 'app_proyecto_medico/graficos.html', context)
+
+
+def context_lista_examen():
+    filename = "/app_proyecto_medico/data/examen.json"
+    with open(str(settings.BASE_DIR)+filename, 'r') as file:
+        formulario = json.load(file)
     context = {'lista_examen': formulario['formulario']}
     return context
+
+
 def examenes(request):
     if request.method == "GET":
         formulario = FormularioExamen()
-        context = {'formulario':formulario}
+        context = {'formulario': formulario}
         context.update(context_lista_examen())
         print("El GET de examenes contiene", context)
-        
+
         return render(request, 'app_proyecto_medico/examenes.html', context)
-    
+
     elif request.method == "POST":
         print("El POST contiene: ", request.POST)
         formulario_devuelto = FormularioExamen(request.POST)
         print("Este es el formulario devuelto: ", formulario_devuelto)
-        
+
         if formulario_devuelto.is_valid() == True:
             datos_formulario = formulario_devuelto.cleaned_data
             print("Los datos limpios del formulario son: ", datos_formulario)
             filename = "/app_proyecto_medico/data/examen.json"
             with open(str(settings.BASE_DIR)+filename, 'r') as file:
-                data=json.load(file)
-                nuevo_ultimo_identificador = int(data['ultimo_identificador'])+1
+                data = json.load(file)
+                nuevo_ultimo_identificador = int(
+                    data['ultimo_identificador'])+1
                 data['ultimo_identificador'] = nuevo_ultimo_identificador
                 datos_formulario['identificador'] = nuevo_ultimo_identificador
                 data['formulario'].append(datos_formulario)
@@ -52,21 +109,26 @@ def examenes(request):
                 json.dump(data, file)
             return redirect('app_proyecto_medico:examenes')
         else:
-            context = {'formulario':formulario_devuelto}
+            context = {'formulario': formulario_devuelto}
             return render(request, 'app_proyecto_medico/examenes.html', context)
+
+
 def lista_examen(request):
     context = context_lista_examen()
     return render(request, 'app_proyecto_medico/examen.html', context)
 
+
 def agendar(request):
     return render(request, 'app_proyecto_medico/agendar.html')
+
+
 def registro(request):
     if request.method == "GET":
         formulario = FormularioRegistro()
-        context = {'formulario':formulario}
+        context = {'formulario': formulario}
         print("El GET contiene", context)
         return render(request, 'app_proyecto_medico/registro.html', context)
-    
+
     elif request.method == "POST":
         print("El POST contiene: ", request.POST)
 
@@ -78,8 +140,9 @@ def registro(request):
             print("Los datos limpios del formulario son: ", datos_formulario)
             filename = "/app_proyecto_medico/data/login.json"
             with open(str(settings.BASE_DIR)+filename, 'r') as file:
-                data=json.load(file)
-                nuevo_ultimo_identificador = int(data['ultimo_identificador'])+1
+                data = json.load(file)
+                nuevo_ultimo_identificador = int(
+                    data['ultimo_identificador'])+1
                 data['ultimo_identificador'] = nuevo_ultimo_identificador
                 datos_formulario['identificador'] = nuevo_ultimo_identificador
                 data['formulario'].append(datos_formulario)
@@ -87,12 +150,14 @@ def registro(request):
                 json.dump(data, file)
             return redirect('app_proyecto_medico:login')
         else:
-            context = {'formulario':formulario_devuelto}
+            context = {'formulario': formulario_devuelto}
             return render(request, 'app_proyecto_medico/registro.html', context)
+
+
 def login(request):
     if request.method == "GET":
         formulario = FormularioLogin()
-        context = {'formulario':formulario}
+        context = {'formulario': formulario}
         print("El GET contiene", context)
         return render(request, 'app_proyecto_medico/login.html', context)
 
@@ -100,40 +165,43 @@ def login(request):
         print("El POST contiene:", request.POST)
 
         formulario_devuelto = FormularioLogin(request.POST)
-        print("Este es el formulario devuelto: ",formulario_devuelto)
+        print("Este es el formulario devuelto: ", formulario_devuelto)
         if formulario_devuelto.is_valid() == True:
-            
+
             datos_formulario = formulario_devuelto.cleaned_data
-            
+
             print("Los datos limpios del formulario son:", datos_formulario)
-            filename="/app_proyecto_medico/data/login.json"
+            filename = "/app_proyecto_medico/data/login.json"
             with open(str(settings.BASE_DIR)+filename, 'r') as file:
-                data=json.load(file)
-                print("Esto es data",data)
+                data = json.load(file)
+                print("Esto es data", data)
             for form in datos_formulario:
                 print("Primer for", form)
-                for  formulario in data['formulario']:
+                for formulario in data['formulario']:
                     print("Segundo for")
                     print("Este es el json:", formulario)
                     print("Este es el formulario:", datos_formulario)
                     if datos_formulario['email'] == formulario['email'] and formulario['password'] == datos_formulario['password']:
-                        
+
                         print("El email si existe", datos_formulario)
-                        return redirect('app_proyecto_medico:privada') 
+                        return redirect('app_proyecto_medico:privada')
                     else:
                         print("El email no existe")
-            context ={'formulario':formulario_devuelto}
+            context = {'formulario': formulario_devuelto}
             return render(request, 'app_proyecto_medico/login.html', context)
         else:
-            context ={'formulario':formulario_devuelto}
+            context = {'formulario': formulario_devuelto}
             return render(request, 'app_proyecto_medico/login.html', context)
 
+
 def context_lista_pacientes():
-    filename= "/app_proyecto_medico/data/pacientes.json"
+    filename = "/app_proyecto_medico/data/pacientes.json"
     with open(str(settings.BASE_DIR)+filename, 'r') as file:
-        pacientes=json.load(file)
+        pacientes = json.load(file)
     context = {'lista_paciente': pacientes['paciente']}
     return context
+
+
 def crear_paciente(request):
     if request.method == "GET":
         formulario = FormularioPaciente()
@@ -149,43 +217,64 @@ def crear_paciente(request):
 
         if formulario_devuelto.is_valid() == True:
             datos_formulario = formulario_devuelto.cleaned_data
-            datos_formulario['fecha_nacimiento']=datos_formulario['fecha_nacimiento'].strftime("%Y-%m-%d")
+            datos_formulario['fecha_nacimiento'] = datos_formulario['fecha_nacimiento'].strftime(
+                "%Y-%m-%d")
             print("Los datos limpios del formulario son:", datos_formulario)
-            filename= "/app_proyecto_medico/data/pacientes.json"
+            filename = "/app_proyecto_medico/data/pacientes.json"
             with open(str(settings.BASE_DIR)+filename, 'r') as file:
-                data=json.load(file)
-                nuevo_ultimo_identificador = int(data['ultimo_identificador']) + 1
+                data = json.load(file)
+                nuevo_ultimo_identificador = int(
+                    data['ultimo_identificador']) + 1
                 data['ultimo_identificador'] = nuevo_ultimo_identificador
                 datos_formulario['identificador'] = nuevo_ultimo_identificador
                 data['paciente'].append(datos_formulario)
             with open(str(settings.BASE_DIR)+filename, 'w') as file:
                 json.dump(data, file)
-            
+
             return redirect('app_proyecto_medico:crear_paciente')
         else:
             context = {'formulario': formulario_devuelto}
             context.update(context_lista_pacientes())
             return render(request, 'app_proyecto_medico/crear_paciente.html', context)
+
+
 def lista_paciente(request):
     context = context_lista_pacientes()
-    print("este es el context de lista pacientes",context)
+    print("este es el context de lista pacientes", context)
     return render(request, 'app_proyecto_medico/lista_paciente.html', context)
-    
+
+
 def eliminar_paciente(request, identificador):
     if request.method == "GET":
         context = {'identificador': identificador}
         return render(request, "app_proyecto_medico/eliminar_paciente.html", context)
 
     if request.method == "POST":
-        filename= "/app_proyecto_medico/data/pacientes.json"
+        filename = "/app_proyecto_medico/data/pacientes.json"
         with open(str(settings.BASE_DIR)+filename, 'r') as file:
-            data=json.load(file)
+            data = json.load(file)
         for formulario in data['paciente']:
-            if int(formulario['identificador'])==int(identificador):
+            if int(formulario['identificador']) == int(identificador):
                 data['paciente'].remove(formulario)
                 break
         with open(str(settings.BASE_DIR)+filename, 'w') as file:
             json.dump(data, file)
-        
+
         return redirect('app_proyecto_medico:lista_paciente')
 
+def eliminar_examen(request, identificador):
+    if request.method == 'GET':
+        context ={'identificador':identificador}
+        return render(request, 'app_proyecto_medico/eliminar_examen.html', context)
+    
+    if request.method == 'POST':
+        filename ='/app_proyecto_medico/data/examen.json'
+        with open(str(settings.BASE_DIR)+filename, 'r') as file:
+            data=json.load(file)
+        for formulario in data['formulario']:
+            if int(formulario['identificador']) == int(identificador):
+                data['formulario'].remove(formulario)
+                break
+        with open(str(settings.BASE_DIR)+filename, 'w') as file:
+            json.dump(data, file)
+        return redirect('app_proyecto_medico:examenes')
